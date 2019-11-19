1674ee709e9068fa844f1b410b79b3947a6d74b0
guanpeizuo <peizuo.guan@mail.hypers.com>
2019-11-19 12:27:36 +0800

    update pom

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
@@ -9,6 +11,7 @@ import java.util.Date;
 @RestController
 public class HelloController {
 
+    @RequestMapping("/index")
     public String index(){
 
         return "Hello, " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
